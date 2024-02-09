f66e507d63f5b22699fc23195d66e1c7437482a7
agmahaha <antonio_mendoza@dlsu.edu.ph>
2024-02-09 17:30:13 +0800

    next.js for ssr

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import styles from "./page.module.css";
+
+const Navbar = () => {
+  return (
+    <div className={styles.navbar}>
+        <div>
+            login
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
