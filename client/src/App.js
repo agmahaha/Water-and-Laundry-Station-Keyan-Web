28b84e93e5cc1bcd13ae1e1c13d11b624df66dcd
agmahaha <antonio_mendoza@dlsu.edu.ph>
2024-02-11 23:24:07 +0800

    route for ownerView page

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,11 @@ import FormLog from "./pages/loginPage"
 import Home from './pages/homePage'
 import Service from './pages/servicesPage'
 import About from'./pages/aboutPage'
+import OwnerView from './pages/ownerView'
+import PageNotFound from "./pages/pageNotFound"
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css';
-import PageNotFound from "./pages/pageNotFound"
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         <Routes>
           <Route path ="/" element={<Home/>}/>
           <Route path ="/login" element={<FormLog/>}/>
+          <Route path ="/homepage" element={<OwnerView/>}/>
           <Route path ="*" element={<PageNotFound/>}/>
         </Routes>
       </BrowserRouter>
