f5d81b0121a3210bfa47fee9cbc4393c9877b5a9
plipdra <philippe_nikos_edralin@dlsu.edu.ph>
2024-02-12 14:01:40 +0800

    added logged in navbar state

---
+++ client/src/components/NavbarLogged/index.jsx
@@ -0,0 +1,86 @@
+import React, {useEffect, useState} from 'react'
+import {AppBar, Toolbar, Typography, Tabs, Tab, Avatar }from '@mui/material';
+import { useNavigate, useLocation } from 'react-router';
+import Logo from './whiteLogoKeyan.PNG'
+
+const NavLoggedIn = () => {
+    const [value, setValue] = useState();
+    const navigate = useNavigate()
+    const location = useLocation()
+    
+    useEffect(() => {
+        if (location.pathname === '/about') {
+          setValue(0);
+        } else if (location.pathname === '/services') {
+          setValue(1);
+        } else if (location.pathname === '/login') {
+          setValue(2);
+        }
+      }, [location.pathname]);
+    
+      const handleChange = (event, newValue) => {
+        setValue(newValue);
+    
+        switch (newValue) {
+          case 0:
+            navigate('/about');
+            break;
+          case 1:
+            navigate('/services');
+            break;
+          case 2:
+            navigate('/login');
+            break;
+          default:
+            navigate('/');
+        }
+      };
+    
+
+  return (
+    <React.Fragment>
+        <AppBar
+            sx ={{background:'#0b4c84', 
+            margin:"10px auto",
+            borderRadius:"10px",
+            position:'fixed',
+            width: "85%",
+            left: 0,
+            right: 0,
+            }}>
+            <Toolbar>
+                <Typography
+                    onClick={() => {
+                        navigate('/')
+                      } }
+                      sx={{
+                        color: 'White',
+                        "&:hover": {
+                          cursor: "pointer",
+                        },
+                      }}
+                >
+                    <img
+                    src={Logo}
+                    alt="Logo"
+                    style={{ height: '60px', width: '60px' }} />
+                </Typography>
+                <Tabs
+                    sx ={{marginLeft:"auto"}} 
+                    textColor='inherit'
+                    value={value}
+                    onChange={handleChange}
+                    TabIndicatorProps={{style: {background:'#F4A4AC'}}}
+                    >
+                        <Tab label ="About" sx={{ color: "white" }}/>
+                        <Tab label ="Services" sx={{ color: "white" }}/>
+                        <Avatar>U</Avatar>
+                </Tabs>
+            </Toolbar>
+        </AppBar>
+    </React.Fragment>
+
+  )
+}
+
+export default NavLoggedIn
--- client/src/pages/ownerView/index.jsx
+++ client/src/pages/ownerView/index.jsx
@@ -1,15 +1,18 @@
 import Pricelist from "./price.png";
 import Order from "./order.png";
 import { Box, Grid, Typography, useTheme} from '@mui/material'
-import Navbar from '../../components/Navbar'
+// import Navbar from '../../components/Navbar'
+import NavLoggedIn from '../../components/NavbarLogged'
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const OwnerView = () => {
     const theme = useTheme();
+    const navigate = useNavigate();
 
     return (
         <>
-            <Navbar />
+            <NavLoggedIn />
             <Box
                 style={{ marginTop: '80px', padding: '2% 10% 5% 10%', alignItems: 'center', }}
                 sx={{
@@ -34,6 +37,7 @@ const OwnerView = () => {
                     <Grid item xs={12} sm={6} sx={{ textAlign: 'right' }}>
                         {/* Content for the first column */}
                         <Box
+                            
                             sx={{
                                 display: 'flex',
                                 flexDirection: 'column',
