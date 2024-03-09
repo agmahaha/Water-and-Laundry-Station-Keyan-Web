78c46268799b481512ed14c9d3b435ee8d16693c
agmahaha <antonio_mendoza@dlsu.edu.ph>
2024-03-10 00:42:26 +0800

    initial for user schema

---
+++ server/controller/auth.js
@@ -0,0 +1,48 @@
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import User from "../models/users.js"
+
+export const registerUser = async (req, res) => {
+    try{
+        const{
+            username,
+            email,
+            password
+        } = req.body
+
+        const hash = await bcrypt.hash(password, 10)
+
+        const newUser = new User({
+            username,
+            email,
+            password: hash
+        })
+
+        const savedUser = await newUser.save()
+        res.status(201).json(savedUser)
+
+    }catch{
+        res.status(500).json({ error: err.message })
+    }
+}
+
+export const loginUser = async (req, res) => {
+    try{
+        const {username, passowrd} = req.body
+        const user = await User.findOne({username: username})
+
+        if(!user)
+            return res.Status(400).json({msg: "User not Found"})
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if(!isMatch)
+            return res.Status(400).json({msg: "Password Invalid"})
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+        delete user.password
+        res.status(200).json({token, user})
+    }catch{
+        res.status(500).json({ error: err.message })
+    }
+}
---
+++ server/route/auth.js
@@ -0,0 +1,7 @@
+import express from "express"
+import {login} from "../controller/auth.js"
+
+const router = express.Router()
+
+router.post("/loginUser", login)
+export default router
