f856c0f1b435d3b649a266a9d4529b645501610b
plipdra <philippe_nikos_edralin@dlsu.edu.ph>
2024-03-11 19:30:21 +0800

    fixed services

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,129 +1,123 @@
 import React, {useEffect, useState} from 'react'
-import {AppBar, Toolbar, Typography, Tabs, Tab, MenuItem, Menu,}from '@mui/material';
+import {AppBar, Toolbar, Typography, Tabs, Tab, MenuItem, Menu, Select, InputBase, FormControl, InputLabel, Input}from '@mui/material';
 import { useNavigate, useLocation } from 'react-router';
 import Logo from './keyanwhite.PNG'
 import Logo2 from './pinkLogoKeyan.PNG'
 
 const Navbar = () => {
-    const [value, setValue] = useState();
-    const [anchorE1, setAnchorE1] = useState(false)
-    const open =  Boolean(anchorE1)
-    const [hovering, setHovering] = useState(false);
-    const navigate = useNavigate()
-    const location = useLocation()
+  const [value, setValue] = useState();
+  const [anchorE1, setAnchorE1] = useState(false)
+  const open =  Boolean(anchorE1)
+  const [hovering, setHovering] = useState(false);
+  const navigate = useNavigate()
+  const location = useLocation()
 
-    useEffect(() => {
-        if (location.pathname === '/about') {
-          setValue(0);
-        } else if (location.pathname === '/announcements') {
-          setValue(1);
-        } else if (location.pathname === ('/water' || '/laundry')) {
-          setValue(2);
-        } else if (location.pathname === '/login') {
-          setValue(3);
-        } 
-      }, [location.pathname]);
-    
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    
-        switch (newValue) {
-          case 0:
-            navigate('/about');
-            break;
-          case 1:
-            navigate('/announcements');
-            break;
-          case 2:
-            setAnchorE1(!anchorE1)
-            break;
-          case 3:
-            navigate('/login');
-            break;
-          default:
-            navigate('/');
-        }
-      };
-    const handleClose = () =>{
-      setAnchorE1(null)
+  useEffect(() => {
+    if (location.pathname === '/about') {
+      setValue(0);
+    } else if (location.pathname === '/announcements') {
+      setValue(1);
+    } else if (location.pathname === ('/water' || '/laundry')) {
+      setValue(2);
+    } else if (location.pathname === '/login') {
+      setValue(3);
+    } 
+  }, [location.pathname]);
+  
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+
+    switch (newValue) {
+      case 0:
+        navigate('/about');
+        break;
+      case 1:
+        navigate('/announcements');
+        break;
+      case 2:
+        setAnchorE1(!anchorE1)
+        break;
+      case 3:
+        navigate('/login');
+        break;
+      default:
+        navigate('/');
     }
+  };
 
-    const handleOpen= () =>{
-      setAnchorE1(true)
-    }
+return (
+  <React.Fragment>
+    <AppBar
+        sx ={{background:'#0b4c84', 
+        margin:"10px auto",
+        borderRadius:"10px",
+        position:'sticky',
+        width: "85%",
+        left: 0,
+        right: 0,
+      }}>
+      <Toolbar>
+        <Typography
+            onClick={() => {
+                navigate('/')
+              } }
+              sx={{
+                color: 'White',
+                "&:hover": {
+                  cursor: "pointer",
+                },
+              }}
+            onMouseOver={() => setHovering(true)}
+            onMouseOut={() => setHovering(false)}
+        >
+          <img
+          src={hovering ? Logo2 : Logo}
+          alt="Logo"
+          style={{ height: '60px', width: '60px' }}
+          />
+        </Typography>
+        <Tabs
+            sx ={{marginLeft:"auto"}} 
+            textColor='inherit'
+            value={value}
+            onChange={handleChange}
+            TabIndicatorProps={{style: {background:'#F4A4AC'}}}
+          >
+            <Tab label ="About" sx={{ color: "white", "&:hover": {color: "#F4A4AC"}}}/>
+            <Tab label ="Announcements" sx={{ color: "white", "&:hover": {color: "#F4A4AC"}}}/>
+            <FormControl>
+              <Select
+                sx={{
+                    backgroundColor: '#0b4c84',
+                    width: "140px",
+                    borderRadius: "0.25rem",
+                    height: "50px",
+                    p: "0.25rem 1rem",
+                    "& .MuiSvgIcon-root:": {
+                        pr: "0.25rem",
+                        width: "3rem"
+                    },
+                    color: "white", "&:hover": {color: "#F4A4AC"}
+                }}    
+                maxMenuHeight={1}
+                defaultValue="none"
+                displayEmpty
+                input={<InputBase/>}
+              >
+                <MenuItem key='0' disabled value="none"><Typography sx={{ color: "#9DB7CD", "&:hover": {color: "#F4A4AC"}}}>SERVICES</Typography></MenuItem>
+                <MenuItem onClick={() => {navigate(`/water`)}}>
+                    <Typography>Water</Typography>
+                </MenuItem>
+                <MenuItem onClick={() => {navigate("/laundry")}}><Typography>Laundry</Typography></MenuItem>
+              </Select>
+            </FormControl>
+            {/* </Tab> */}
+            <Tab label ="Login/Signup" sx={{ color: "white", "&:hover": {color: "#F4A4AC"}}}/>
 
-  return (
-    <React.Fragment>
-        <AppBar
-            sx ={{background:'#0b4c84', 
-            margin:"10px auto",
-            borderRadius:"10px",
-            position:'sticky',
-            width: "85%",
-            left: 0,
-            right: 0,
-            }}>
-            <Toolbar>
-                <Typography
-                    onClick={() => {
-                        navigate('/')
-                      } }
-                      sx={{
-                        color: 'White',
-                        "&:hover": {
-                          cursor: "pointer",
-                        },
-                      }}
-                    onMouseOver={() => setHovering(true)}
-                    onMouseOut={() => setHovering(false)}
-                >
-                    <img
-                    src={hovering ? Logo2 : Logo}
-                    alt="Logo"
-                    style={{ height: '60px', width: '60px' }}
-                    />
-                </Typography>
-                <Tabs
-                    sx ={{marginLeft:"auto"}} 
-                    textColor='inherit'
-                    value={value}
-                    onChange={handleChange}
-                    TabIndicatorProps={{style: {background:'#F4A4AC'}}}
-                    >
-                        <Tab label ="About" sx={{ color: "white", "&:hover": {color: "#F4A4AC"}}}/>
-                        <Tab label ="Announcements" sx={{ color: "white", "&:hover": {color: "#F4A4AC"}}}/>
-                        <Tab label ="Services" 
-                        id='water_or_laundry'
-                        onClick={handleOpen}
-                        aria-controls={open ? 'water_or_laundry' : undefined}
-                        aria-haspopup= 'true'
-                        aria-expanded= {open ? true : undefined}
-                        sx={{ color: "white", "&:hover": {color: "#F4A4AC"}}}>
-
-                          
-                        </Tab>
-                        <Tab label ="Login/Signup" sx={{ color: "white", "&:hover": {color: "#F4A4AC"}}}/>
-
-                        </Tabs>
-            </Toolbar>
-            <Menu id= 'water_or_laundry' 
-                              anchorEl={{anchorE1}} 
-                              open = {open} 
-                              MenuListProps={{
-                                'aria-labelledby' : 'water_or_laundry'
-                              }}
-                              onClose={handleClose}
-                              sx ={{marginRight:'auto'}}>
-                              <MenuItem onClick={() => {
-                                navigate('/water')
-                              } }>Water</MenuItem>
-                              <MenuItem onClick={() => {
-                                navigate('/laundry')
-                              } }>Laundry</MenuItem>
-                            </Menu>
-                          
-        </AppBar>                      
-    </React.Fragment>
+          </Tabs>
+      </Toolbar>      
+    </AppBar>                      
+  </React.Fragment>
 
   )
 }
