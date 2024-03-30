6ccde66eefc55ee45d202c0e8b8c771a05634b70
agmahaha <antonio_mendoza@dlsu.edu.ph>
2024-03-30 19:20:01 +0800

    fixed layout for order page

--- client/src/pages/orderPage/index.jsx
+++ client/src/pages/orderPage/index.jsx
@@ -1,13 +1,12 @@
 import React, {useState} from 'react'
-import { Typography, Button, Box, Grid, styled, Paper} from '@mui/material'
-import {Formik} from "formik"
-import * as yup from 'yup'
+import { Typography, Button, Box, Grid} from '@mui/material'
 import Navbar from '../../components/Navbar'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch } from "react-redux";
 
 
 const Order = () => {
+  
     return(
        <><Navbar/>
        <Box
@@ -23,41 +22,142 @@ const Order = () => {
                 }}
         >
             <Grid container>
-                <Grid xs= {6}>
-                    <Grid container sx={{textAlign: 'center'}}>
-                        <Grid xs= {4}>
-                          <Typography>
-                            opt 1
-                          </Typography>
+              <Grid xs = {6} sx= {{borderRight:'2px solid black',}}>
+                <Grid container p={2}>
+                  <Grid xs = {12} sx= {{borderBottom:'2px solid #0B4C84'}}>
+                    <Grid container sx={{textAlign: 'center', marginBottom:'15%'}}>
+                            <Grid xs= {4} >
+                            <Box
+                                sx={{
+                                  margin: 'auto',
+                                  borderRadius: 3,
+                                  bgcolor: '#ffffff',
+                                  height: '300px',
+                                  width: '70%',
+                                  padding: '5%',
+                                  border: '1px solid grey',
+                                  '&:hover': {
+                                    bgcolor: '#7092be',
+                                    transform: 'scale(1.1)',
+                                    cursor: 'pointer',
+                                },
+                                
+                                }}
+
+                                >
+                              <Typography
+                                      sx={{
+                                          fontWeight: 'bold',
+                                          color: 'black',
+                                      }}
+                                  >
+                                      Comforters/Blankets
+                              </Typography>
+                              </Box>
+                            </Grid>
+                            <Grid xs= {4}>
+                            <Box
+                                sx={{
+                                  margin: 'auto',
+                                  borderRadius: 3,
+                                  bgcolor: '#fffff',
+                                  height: '300px',
+                                  width: '70%',
+                                  padding: '5%',
+                                  border: '1px solid grey',
+                                  '&:hover': {
+                                    bgcolor: '#7092be',
+                                    color: "white",
+                                    transform: 'scale(1.1)',
+                                    cursor: 'pointer',
+                                },
+                                
+                                }}
+
+                                >
+                              <Typography
+                                      sx={{
+                                          fontWeight: 'bold',
+                                          color: 'black',
+                  
+                                      }}
+                                  >
+                                      Thick/Linen/Curtains
+                              </Typography>
+                              </Box>
+                            </Grid>
+                            <Grid xs= {4}>
+                            <Box
+                                sx={{
+                                  margin: 'auto',
+                                  borderRadius: 3,
+                                  bgcolor: '#ffffff',
+                                  height: '300px',
+                                  width: '70%',
+                                  padding: '5%',
+                                  border: '1px solid grey',
+                                  '&:hover': {
+                                    bgcolor: '#7092be',
+                                    transform: 'scale(1.1)',
+                                    cursor: 'pointer',
+                                },
+                                
+                                }}
+
+                                >
+                              <Typography
+                                      sx={{
+                                          fontWeight: 'bold',
+                                          color: 'black',
+                                      }}
+                                  >
+                                      Regular Clothing
+                              </Typography>
+                              </Box>
+                            </Grid>
                         </Grid>
-                        <Grid xs= {4}>
-                          <Typography>
-                            opt 2
-                          </Typography>
-                        </Grid>
-                        <Grid xs= {4}>
-                          <Typography>
-                            opt 3
-                          </Typography>
-                        </Grid>
-                    </Grid>
-                
+                  </Grid>
+                  <Grid xs = {12} sx={{textAlign: 'center', marginTop:'15%'}}>
+                    <Typography>sample 3</Typography>
+                  </Grid>
                 </Grid>
-                <Grid xs= {6}>
-                    <h1>Guidelines</h1>
-                    <Typography>Lorem ipsum dolor sit amet 
+              </Grid>
+              <Grid xs = {6}>
+                <Grid container p = {2}>
+                  <Grid xs = {12} sx= {{borderBottom:'2px solid #0B4C84'}}>
+                    <h1> General Guidelines:</h1>
+                    <Typography> • Lorem ipsum dolor sit amet 
                         consectetur adipisicing elit. Odit 
                         eaque ex eius? Quisquam enim consectetur 
                         iure dolore a unde quas fuga fugit molestias 
                         maiores quam, eos aspernatur! Distinctio, 
                         quod facilis.</Typography>
+                    <Typography> • Lorem ipsum dolor sit amet 
+                        consectetur adipisicing elit. Odit 
+                        eaque ex eius? Quisquam enim consectetur 
+                        iure dolore a unde quas fuga fugit molestias 
+                        maiores quam, eos aspernatur! Distinctio, 
+                        quod facilis.</Typography>
+                    <Typography> • Lorem ipsum dolor sit amet 
+                        consectetur adipisicing elit. Odit 
+                        eaque ex eius? Quisquam enim consectetur 
+                        iure dolore a unde quas fuga fugit molestias 
+                        maiores quam, eos aspernatur! Distinctio, 
+                        quod facilis.</Typography>
+                    <Typography sx= {{ marginBottom:'15%'}}> 
+                        • Lorem ipsum dolor sit amet 
+                        consectetur adipisicing elit. Odit 
+                        eaque ex eius? Quisquam enim consectetur 
+                        iure dolore a unde quas fuga fugit molestias 
+                        maiores quam, eos aspernatur! Distinctio, 
+                        quod facilis.</Typography>
+                  </Grid>
+                <Grid xs = {12} sx={{marginTop:'2%'}}>
+                  <h2>PRICE BREAKDOWN:</h2>
                 </Grid>
-                <Grid xs= {6}>
-                    <Typography>sample 3</Typography>
+              </Grid>
                 </Grid>
-                <Grid xs= {6}>
-                    <Typography>PRICE BREAKDOWN</Typography>
-                </Grid>
+                
                 
             </Grid>
 
